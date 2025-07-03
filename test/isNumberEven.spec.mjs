54699edb656baaa0885fd6a2c37f77a9a8809cc8
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-07-03 14:31:47 +0500

    update isNumberEven.spec.mjs

--- test/isNumberEven.spec.mjs
+++ test/isNumberEven.spec.mjs
@@ -12,4 +12,7 @@ describe("isNumberEven positive scenaries", function () {
   it("should return true if number is even", function () {
     expect(validator.isNumberEven(4)).to.be.equal(true);
   });
+  it('should return false if number is odd', function() {
+    expect(validator.isNumberEven(5)).to.be.equal(false)
+  })
 });
